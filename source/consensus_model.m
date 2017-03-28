e537cc0485db160c9f8ab4bd2edd84bc25ce9742
Alvarojaf <ajota35@gmail.com>
2017-03-27 20:48:29 -0500

    código con un consensus de seis agentes a un valor de referencia deseado

---
+++ source/consensus_model.m
@@ -0,0 +1,22 @@
+function dx=consensus_model(t,x,~,a,r)
+% consensus_model  Es una función que toma los valores de estado (x),
+% parámetros adicionales (a,r) y entrega los valores de las dinámicas del
+% sistema (dx). usualmente es usada en la solución de ecuaciones
+% diferenciales.
+
+dx=zeros(6,1); %inicialización derivadas
+
+for i=1:1:6 % recorre estados generando \dot{x}
+    sum=0; %definicion del valor inicial e la sumatoria
+    b=0;
+    for j=1:1:6 % recorre los agentes vecinos
+        if a(i,j)~=0
+            sum=sum+a(i,j)*(x(j)-x(i));
+            
+        end        
+    end
+    dx(i)=sum+a(i,7)*(r-x(i)); %dínamicas de consesus follower leader
+end
+
+
+
